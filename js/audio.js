0d8faf8d50301c0f46790c67162d09ca04686e9f
MolnarHangaBorbala <molnarhanga4@gmail.com>
2025-08-19 21:41:16 +0200

    Delete js directory

--- js/audio.js
+++
@@ -1,9 +0,0 @@
-const clickSound = document.getElementById("clickSound");
-const targets = document.querySelectorAll('.sound-class');
-
-targets.forEach(item => {
-    item.addEventListener("click", () => {
-        clickSound.currentTime = 0;
-        clickSound.play();
-    });
-});
